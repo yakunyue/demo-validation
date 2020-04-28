1783422ba8abd792625696994047dd8f1b20cd97
yueyakun <yyk_job@163.com>
2020-04-28 10:57:17 +0800

    测试错误信息消息占位符

--- src/main/java/com/fxg/demo/validation/validator/CheckCaseValidator.java
+++ src/main/java/com/fxg/demo/validation/validator/CheckCaseValidator.java
@@ -1,6 +1,7 @@
 package com.fxg.demo.validation.validator;
 
 import com.fxg.demo.validation.annotation.CheckCase;
+import org.hibernate.validator.constraintvalidation.HibernateConstraintValidatorContext;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
@@ -19,6 +20,8 @@ public class CheckCaseValidator implements ConstraintValidator<CheckCase, String
 
 	@Override
 	public boolean isValid(String v, ConstraintValidatorContext context) {
+		HibernateConstraintValidatorContext hibernateContext = context.unwrap(HibernateConstraintValidatorContext.class);
+		hibernateContext.addMessageParameter("zhMode", caseMode == CheckCase.CaseMode.UPPER ? "大写" : "小写"); // 友好展示
 		return this.caseMode.getAdapter().adaptive(v);
 	}
 }
